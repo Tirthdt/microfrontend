56865bd1530bd5a56321c315fc72bb1aaa852116
Tirthdt <tirtht2@gmail.com>
2022-10-26 10:44:41 +0530

    Folder change

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 const App = () => {
   return (
     <div>
-      Container app
+      Container Deployed app
       <hr />
       <MarketingApp />
     </div>
