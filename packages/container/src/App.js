e080410e25b896ff272cb9625aa8f433f0deb24d
Tirthdt <tirtht2@gmail.com>
2022-10-26 18:28:01 +0530

    Styles clash issue fix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
