0bb1583b971b2ffad1929df8459c90ed9958a859
Tirthdt <tirtht2@gmail.com>
2022-10-26 10:38:44 +0530

    Variables changes for workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 const App = () => {
   return (
     <div>
-      Container Application
+      Container
       <hr />
       <MarketingApp />
     </div>
